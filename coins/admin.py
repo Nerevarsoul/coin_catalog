68a660c3edd7032946e879ad0332d7557c83b267
Neri <keshaeverywhere@gmail.com>
2015-12-11 22:39:19 +0300

    Register Profile model as inline for User.

--- coins/admin.py
+++ coins/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .models import *
+
+
+class UserInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Information'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserInline,)
 
 
 # Register your models here.
@@ -8,4 +20,5 @@ admin.site.register(Coins)
 admin.site.register(CatalogCoins)
 admin.site.register(Country)
 admin.site.register(Address)
-admin.site.register(UserProfile)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)

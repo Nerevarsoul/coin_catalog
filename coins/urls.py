219d8af494cb1a65a98fa90a093618dc75cb698c
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2015-12-15 14:16:37 +0300

    Update urls.py

--- coins/urls.py
+++ coins/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import include, url
 
-from .views import IndexView, CoinsTableView, CreateCoinsView, UpdateUserView,\
+from .views import IndexView, CatalogCoinsTableView, CreateCatalogCoinsView, UpdateUserView,\
     CatalogCoinsDetailView
 from .vk import vkontakte_view
 
 
 urlpatterns = [
     url(r'^index/$', IndexView.as_view(), name = 'index'),
-    url(r'^coins/$', CoinsTableView.as_view(), name = 'coins'),
-    url(r'^create_coins/$', CreateCoinsView.as_view(), name = 'create_coins'),
-    url(r'^update_profile/$', UpdateUserView.as_view(), name = 'create_coins'),
-    url(r'^coin_detail/(?P<pk>\d+)/$', CatalogCoinsDetailView.as_view(), name = 'detail_coins'),
+    url(r'^catalogue/$', CatalogCoinsTableView.as_view(), name = 'catalogue'),
+    url(r'^create_catalog_coins/$', CreateCoinsView.as_view(), name = 'create_catalog_coins'),
+    url(r'^update_profile/$', UpdateUserView.as_view(), name = 'update_profile'),
+    url(r'^catalog_coin_detail/(?P<pk>\d+)/$', CatalogCoinsDetailView.as_view(), 
+        name = 'catalog_detail_coins'),
     url(r'^vk/', vkontakte_view, name='vk_app'),
     url(r'', include('social_auth.urls')),
 ]

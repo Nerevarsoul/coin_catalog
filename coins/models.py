ef95937efb9589416e7254fa9129d11eceae218a
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-01-12 11:46:19 +0300

    Update models.py

--- coins/models.py
+++ coins/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
 
 COUNTRY_STATUS = (
@@ -22,11 +25,20 @@ class CatalogCoins(models.Model):
     circulation = models.IntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     catalog_image = models.ManyToManyField(Image)
-
+    slug = models.SlugField(max_length=150)
+    
     def __unicode__(self):
         return "{} {}".format(self.face_value, self.currency)
-
-
+    
+    
+@receiver(pre_save, sender=CatalogCoins)
+def catalog_get_slug(sender, instance, **kwargs):
+    instance.slug = slugify("{} {} {} {}".format(instance.face_value, 
+                                                 instance.currency, 
+                                                 instance.circulation, 
+                                                 instance.country)) 
+    
+    
 class Coins(models.Model):
 
     condition = models.CharField(max_length=50, blank=True, null=True)

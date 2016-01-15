f685ef3d3dbd5d74af222019a18c2b60fc2199d2
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-01-15 11:34:19 +0300

    Update urls.py

--- coins/urls.py
+++ coins/urls.py
@@ -8,6 +8,7 @@ from .vk import vkontakte_view
 urlpatterns = [
     url(r'^index/$', IndexView.as_view(), name = 'index'),
     url(r'^catalogue/$', CatalogCoinsTableView.as_view(), name = 'catalogue'),
+    url(r'^catalogue/(?P<country>\d+)/$', CatalogCoinsCountryiew.as_view(), name = 'catalogue_country'),
     url(r'^create_catalog_coins/$', CreateCoinsView.as_view(), name = 'create_catalog_coins'),
     url(r'^update_profile/$', UpdateUserView.as_view(), name = 'update_profile'),
     url(r'^catalog_coin_detail/(?P<slug>\d+)/$', CatalogCoinsDetailView.as_view(), 

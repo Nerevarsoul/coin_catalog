cbd731bc6741cc8e9cfb1feedb7f433206dd7488
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-01-19 11:52:28 +0300

    Use unidecode for slugify

--- coins/signals.py
+++ coins/signals.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8  -*-
 from __future__ import unicode_literals
 
+import unidecode
+
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
@@ -10,13 +12,13 @@ from .models import CatalogCoins, Country
 
 @receiver(pre_save, sender=CatalogCoins)
 def catalog_get_slug(sender, instance, **kwargs):
-    instance.slug = slugify("{} {} {} {}".format(instance.face_value, 
-                                                 instance.currency, 
-                                                 instance.circulation, 
-                                                 instance.country))
+    instance.slug = slugify("{} {} {} {}".format(unidecode(instance.face_value), 
+                                                 unidecode(instance.currency), 
+                                                 unidecode(instance.circulation), 
+                                                 unidecode(instance.country)))
                  
                                                  
 @receiver(pre_save, sender=Country)
 def country_get_slug(sender, instance, **kwargs):
-    instance.slug = slugify(instance.name)                                               
+    instance.slug = slugify(unidecode(instance.name))                                               
 

a6c23837bafd3bcc5c57a7203f63ab588105f751
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-01-29 18:19:00 +0300

    Update urls.py

--- coins/urls.py
+++ coins/urls.py
@@ -7,14 +7,28 @@ from .vk import vkontakte_view
 
 urlpatterns = [
     url(r'^index/$', IndexView.as_view(), name='index'),
+    
+    # catalogue
     url(r'^catalogue/$', CatalogCoinsTableView.as_view(), name='catalogue'),
     url(r'^catalogue/(?P<country>\d+)/$', CatalogCoinsCountryView.as_view(),
         name='catalogue_country'),
     url(r'^create_catalog_coins/$', CreateCatalogCoinsView.as_view(),
         name='create_catalog_coins'),
-    url(r'^update_profile/$', UpdateUserView.as_view(), name='update_profile'),
     url(r'^catalog_coin_detail/(?P<slug>\d+)/$', CatalogCoinsDetailView.as_view(), 
         name='catalog_detail_coins'),
+    
+    # user    
+    url(r'^update_profile/$', UpdateUserView.as_view(), name='update_profile'),
+    
+    # coins
+    url(r'^coins/$', CoinsTableView.as_view(), name='catalogue'),
+    url(r'^coins/(?P<country>\d+)/$', CoinsCountryView.as_view(),
+        name='coins_country'),
+    url(r'^create_coins/$', CreateCoinsView.as_view(),
+        name='create_coins'),
+    url(r'^coin_detail/(?P<slug>\d+)/$', CoinsDetailView.as_view(), 
+        name='detail_coins'),
+    
     url(r'^vk/', vkontakte_view, name='vk_app'),
     url(r'', include('social_auth.urls')),
 ]

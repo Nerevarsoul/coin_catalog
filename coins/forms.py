c318497787279745b77ba74ec25786df573b2120
Neri <keshaeverywhere@gmail.com>
2016-03-29 22:12:28 +0300

    Edit forms after add apps.

--- coins/forms.py
+++ coins/forms.py
@@ -5,27 +5,10 @@ from django import forms
 
 from ajax_select.fields import AutoCompleteSelectField, AutoCompleteSelectMultipleField
 from captcha.fields import CaptchaField
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Button, Layout
 
-from .models import Coin, CatalogCoin, UserProfile
+from core.forms import CrispyMixin
 
-
-class CrispyMixin(object):
-
-    def __init__(self, *args, **kwargs):
-        super(CrispyMixin, self).__init__(*args, **kwargs)
-        self.helper = FormHelper(self)
-        self.helper.help_text_inline = True
-        self.helper.html5_required = True
-        self.helper.form_class = 'form-horizontal'
-        self.helper.form_method = 'post'
-        self.helper.label_class = 'col-sm-2'
-        self.helper.field_class = 'col-sm-4'
-        button = Button('send_button', self.button_name)
-        button.input_type = 'submit'
-        button.field_classes = 'btn btn-success form-control'
-        self.helper.add_input(button)
+from .models import Coin, CatalogCoin
 
 
 class CatalogCoinsForm(CrispyMixin, forms.ModelForm):
@@ -36,16 +19,6 @@ class CatalogCoinsForm(CrispyMixin, forms.ModelForm):
         model = CatalogCoin
         fields = ("country", "currency", "face_value", "metal",
                   "ruler", "number", "circulation", "description") 
-
-
-class UserProfileForm(CrispyMixin, forms.ModelForm):
-
-    button_name = 'Confirm'
-    captcha = CaptchaField()
-
-    class Meta:
-        model = UserProfile
-        exclude = ('user', )
 
 
 class CoinsForm(CrispyMixin, forms.ModelForm):

69ab5e0d9610f2e56d0fa6c743e3a0e9359c7663
Neri <keshaeverywhere@gmail.com>
2016-03-29 22:14:25 +0300

    Edit admins after add apps.

--- coins/admin.py
+++ coins/admin.py
@@ -1,24 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+
 
 from .models import *
 
 
-class UserInline(admin.StackedInline):
-    model = UserProfile
-    can_delete = False
-    verbose_name_plural = 'Information'
-
-
-class UserAdmin(UserAdmin):
-    inlines = (UserInline,)
-
-
 # Register your models here.
-admin.site.register(Address)
 admin.site.register(CatalogCoin)
 admin.site.register(Coin)
-admin.site.register(Country)
-admin.site.register(Image)
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)

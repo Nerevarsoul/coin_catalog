a874f709c9bf3c33fda75f85e0a6280662d6afec
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-06-23 11:53:56 +0300

    Update production.py

--- mysite/production.py
+++ mysite/production.py
@@ -1,4 +1,5 @@
 from .base import *
+from .secret import *
 
 
 DEBUG = False
@@ -7,3 +8,13 @@ ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASSWORD,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}

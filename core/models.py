fa22ad66d4b79b30c3f38b811404be6e80d4daa5
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-07-04 14:26:10 +0300

    Add abstract model with is_void field

--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8  -*-
+from datetime import datetime
+
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models import signals
 
 
 COUNTRY_STATUS = (
@@ -10,6 +13,46 @@ COUNTRY_STATUS = (
 
 
 # Create your models here.
+class AppQuerySet(models.QuerySet):
+    def delete(self, **kwargs):
+        return self.update(is_void=True)
+
+class AppManager(models.Manager):
+    queryset_class = AppQuerySet
+    use_for_related_fields = True
+
+    def get_queryset(self, exclude_void=True):
+        q = self.queryset_class(self.model)
+        if hasattr(self, 'core_filters'):
+            q = q.filter(
+                **self.core_filters
+            )
+
+        if exclude_void:
+            q = q.exclude(is_void=True)
+        return q
+
+    def all_objects_including_void(self):
+        return self.get_queryset(exclude_void=False)
+        
+        
+class AppModel(models.Model):
+    is_void = models.BooleanField(default=False)
+    date_of_delete = models.DatetimeField()
+    objects = AppManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, **kwargs):
+        self.is_void = True
+        self.date_of_delete = datetime.now()
+        self.save()
+        signals.post_delete.send(
+            sender=self.__class__, instance=self
+        )
+
+
 class Image(models.Model):
 
     image = models.ImageField(upload_to='coin_images',)

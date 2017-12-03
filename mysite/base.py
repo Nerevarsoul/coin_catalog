6388646df48ae96f16285e059ca7778d9a20225d
Nerevarsoul <keshaeverywhere@gmail.com>
2017-12-03 19:10:28 +0300

    Upgrade django to 2.0

--- mysite/base.py
+++ mysite/base.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = (
     'coins',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

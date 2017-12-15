5cc7fd3675eb3960579c854c0df133457678192a
Nerevarsoul <keshaeverywhere@gmail.com>
2017-12-15 09:40:50 +0300

    add mixin for choice create or list serializer

--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'base.html'
+
+
+class GetListOrCreateSerializerMixin(object):
+    serializer_class = ''
+    serializer_class_for_create = ''
+    
+
+    def get_serializer_class(self):
+        if self.method == 'GET':
+            return serializer_class 
+        return serializer_class_for_create
+

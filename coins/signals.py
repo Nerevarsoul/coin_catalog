3881c43fe168c23f4d9262166ec8339980b3051c
Nerevarsoul <keshaeverywhere@gmail.com>
2017-12-20 08:51:16 +0300

    Add pre delete signal

--- coins/signals.py
+++ coins/signals.py
@@ -9,7 +9,15 @@ from .models import CatalogCoin
 
 
 @receiver(post_save, sender=CatalogCoin)
-def catalog_get_slug(sender, instance, created, **kwargs):
+def catalog_coin_post_save_receiver(sender, instance, created, **kwargs):
     if instance.serie and created:
     	instance.serie.amount += 1
     	instance.serie.save()
+
+
+@receiver(pre_delete, sender=CatalogCoin)
+def catalog_coin_pre_delete_receiver(sender, instance, **kwargs):
+    if instance.serie:
+        instance.serie.amount -= 1
+        instance.serie.save()
+

93e55bb2b6ca54d257dc21f234be6f408cc41e7a
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-02-02 23:45:01 +0300

    Add reducers for fetch coins

---
+++ static/assets/js/reducers/index.js
@@ -0,0 +1,61 @@
+import { combineReducers } from 'redux'
+import {
+  SELECT_SERIE,
+  REQUEST_COINS,
+  RECEIVE_COINS
+} from '../actions/coins'
+
+
+function selectedSerie(state = 'reactjs', action) {
+  switch (action.type) {
+    case SELECT_SERIE:
+      return action.serie
+    default:
+      return state
+  }
+}
+
+function coins (
+  state = {
+    isFetching: false,
+    didInvalidate: false,
+    coins: []
+  },
+  action
+) {
+  switch (action.type) {
+    case REQUEST_COINS:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false
+      })
+    case RRECEIVE_COINS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        coins: action.coins,
+        lastUpdated: action.receivedAt
+      })
+    default:
+      return state
+  }
+}
+
+function coinsBySerie(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_POSTS:
+    case REQUEST_POSTS:
+      return Object.assign({}, state, {
+        [action.serie]: coins(state[action.serie], action)
+      })
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  coinsBySerie,
+  selectedSerie
+})
+
+export default rootReducer

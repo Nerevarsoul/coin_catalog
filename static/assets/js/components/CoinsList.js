b80586101f360a02d8dcf68a6a9829fd7889d062
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-02-05 22:51:22 +0300

    Use function component instead class

--- static/assets/js/components/CoinsList.js
+++ static/assets/js/components/CoinsList.js
@@ -1,34 +1,25 @@
 import React from 'react';
 
 
-export default class CoinsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Номинал</th>
-            <th>Год</th>
-            <th>Тема</th>
-            <th>Монетный двор</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.coins.map((coin, index) => 
-            <tr key={index}> 
-              <td>{ `${coin['face_value']} ${coin['currency']}` }</td>
-              <td>{ coin['year']  }</td>
-              <td> { coin['theme']  } </td>
-              <td> { coin['mint']  }  </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    )
-  }
-}
-
+export default const CoinsList = ({ coins }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Номинал</th>
+        <th>Год</th>
+        <th>Тема</th>
+        <th>Монетный двор</th>
+      </tr>
+    </thead>
+    <tbody>
+      {this.props.coins.map((coin, index) =>
+        <tr key={index}>
+          <td>{ `${coin['face_value']} ${coin['currency']}` }</td>
+          <td>{ coin['year']  }</td>
+          <td> { coin['theme']  } </td>
+          <td> { coin['mint']  }  </td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
--- static/assets/js/components/SerieList.js
+++ static/assets/js/components/SerieList.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
 
-export default class SerieList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.series.map((serie, index) => 
-           <li key={index} 
-               onClick={ () => this.props.func(serie['name']) }> 
-             {serie['name']} 
-           </li>
-        )}
-      </div>
-    )
-  }
-}
-
+export default const SerieList = ({ series, func }) => (
+  <div>
+    {this.props.series.map((serie, index) =>
+       <li key={index}
+           onClick={ () => this.props.func(serie['name']) }>
+         {serie['name']}
+       </li>
+    )}
+  </div>
+)

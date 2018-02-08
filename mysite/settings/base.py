b5916ef5c0dde9aaf2674d480e2227af59265e5d
Nerevarsoul <keshaeverywhere@gmail.com>
2018-02-08 09:00:46 +0300

    Solve problem with BASE_DIR after creaion settings folder

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path.cwd()
 
 
 # Quick-start development settings - unsuitable for production

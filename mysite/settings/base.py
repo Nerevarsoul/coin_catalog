57c2b6f834d6a5344f0a0b05c514b9e120b32dcf
Nerevarsoul <keshaeverywhere@gmail.com>
2018-02-08 22:25:18 +0300

    Fix error for determining BASE_PATH

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 from pathlib import Path
 
-BASE_DIR = Path.cwd()
+BASE_DIR = Path(__file__).parents[2]
 
 
 # Quick-start development settings - unsuitable for production

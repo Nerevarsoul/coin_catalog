58cfc29a2e5cc6693cc86232ae8b0f9195611430
Nerevarsoul <keshaeverywhere@gmail.com>
2018-02-13 08:43:09 +0300

    Fix circle import and add cron job

--- coins/apps.py
+++ coins/apps.py
@@ -4,18 +4,18 @@ from django.apps import AppConfig
 
 import django_rq
 
-from .tasks import *
-
 
 class CoinsConfig(AppConfig):
     name = 'coins'
 
     def ready(self):
+        from .tasks import check_serie_amount
+
         scheduler = django_rq.get_scheduler('default')
 
         # Delete any existing jobs in the scheduler when the app starts up
         for job in scheduler.get_jobs():
             job.delete()
 
-        scheduler.schedule(datetime.utcnow(), check_serie_amount, interval=60*5) 
+        scheduler.cron('0 2 * * *', check_serie_amount) 
 

108b1ae14561f049a8263ea096eb16fe755c316f
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-02-17 18:52:51 +0300

    Add settings for rest

--- mysite/settings/rest.py
+++ mysite/settings/rest.py
@@ -7,5 +7,13 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework_filters.backends.DjangoFilterBackend',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
-

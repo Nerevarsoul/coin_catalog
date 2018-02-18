bfa25462188efdffb419148e9bcc8056439d4ebd
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-02-18 13:56:07 +0300

    create spider models

--- spiders/models.py
+++ spiders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from scheduler.models import BaseJob
+from model_utils.models import TimeStampedModel
 
 from core.mixins import ErrorMixin
 
@@ -13,3 +14,16 @@ class SpiderJob(ErrorMixin, BaseJob):
         ordering = ('name',)
 
     result = models.PositiveIntegerField(default=0)
+
+
+class Spider(TimeStampedModel):
+    class Meta:
+        verbose_name = _('Spider')
+        verbose_name_plural = _('Spiders')
+        ordering = ('created',)
+
+    is_active = models.BooleanField(default=False)
+    site_name = models.CharField(max_length=50)
+    url = models.URLField()
+    period = models.CharField(max_length=20)
+    spider_name = models.CharField(max_length=50)

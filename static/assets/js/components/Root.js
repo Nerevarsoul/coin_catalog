083d2ad2b15c06e74b9304fb10339afb10ca4b95
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-03-16 08:54:29 +0300

    Add grid

--- static/assets/js/components/Root.js
+++ static/assets/js/components/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import { Container, Menu } from 'semantic-ui-react';
+import { Container, Menu, Grid } from 'semantic-ui-react';
 
 import { routes, RouteWithSubRoutes } from '../routes';
 
@@ -8,18 +8,26 @@ import { routes, RouteWithSubRoutes } from '../routes';
 const Root = () => (
     <BrowserRouter>
       <Container>
-        <Menu pointing vertical>
-          <Menu.Item link><Link to='/test'>Test</Link></Menu.Item>
-          <Menu.Item link><Link to='/series'>Series</Link></Menu.Item>
-          <Menu.Item link><Link to='/coins'>Coins</Link></Menu.Item>
-        <Menu/>
-      {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            component={route.component}
-          />
-        ))}
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Menu pointing vertical>
+                <Menu.Item link><Link to='/test'>Test</Link></Menu.Item>
+                <Menu.Item link><Link to='/series'>Series</Link></Menu.Item>
+                <Menu.Item link><Link to='/coins'>Coins</Link></Menu.Item>
+              <Menu/>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              {routes.map((route, index) => (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    component={route.component}
+                  />
+                ))}
+            </Grid.Column>
+          <Grid.Row>
+        </Grid>
       </Container>
     </BrowserRouter>
 )

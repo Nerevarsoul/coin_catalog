8dd5dd07a76879d63198ec89cbd897dc13d09ada
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-03-19 22:48:13 +0300

    create form in auth component

--- static/assets/js/components/Auth.js
+++ static/assets/js/components/Auth.js
@@ -2,10 +2,31 @@ import React from 'react';
 import { Input } from 'semantic-ui-react';
 
 
-const AuthComponent = () => (
-  <div>
-    
-  </div>
-)
+export default class AuthComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {login: '', password: ''};
 
-export default AuthComponent
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({event.target.name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Input placeholder='Введите логин...' name='login' value={this.state.login} onChange={this.handleChange} />
+        <Input placeholder='Введите пароль...' name='password' value={this.state.password} onChange={this.handleChange} />
+      </form>
+    );
+  }
+
+}

a27ba665cf9ef36ce6b035dc2efd91f68a58adda
Nerevarsoul <keshaeverywhere@gmail.com>
2018-03-19 22:28:47 +0300

    Add raw-id-mixin to coin models

--- coins/admin.py
+++ coins/admin.py
@@ -1,25 +1,32 @@
 from django.contrib import admin
 
+from dynamic_raw_id.admin import DynamicRawIDMixin
 from mptt.admin import MPTTModelAdmin
 
 from .models import *
 
 
+@admin.register(Serie)
 class SerieAdmin(MPTTModelAdmin):
     list_display = ('name', 'country', 'coin_amount', 'level', 'tree_id',)
+    list_filter = ('country',)
 
 
-class CatalogCoinAdmin(admin.ModelAdmin):
+@admin.register(CatalogCoin)
+class CatalogCoinAdmin(DynamicRawIDMixin, admin.ModelAdmin):
     list_display = ('face_value', 'currency', 'year', 'serie', 'mint',)
     list_select_related = ('serie',)
-    list_filter = (('serie', admin.RelatedOnlyFieldListFilter), 'year')  
+    list_filter = (('serie', admin.RelatedOnlyFieldListFilter), 'year')
+    dynamic_raw_id_fields = ('serie',)  
 
 
-class CoinAdmin(admin.ModelAdmin):
+@admin.register(Coin)
+class CoinAdmin(DynamicRawIDMixin, admin.ModelAdmin):
     list_display = ('catalog_coin', 'get_serie', 'owner', 'get_year', 'status', 'get_mint',)
     list_select_related = ('catalog_coin', 'owner', 'catalog_coin__serie',)
     list_filter = (('catalog_coin__serie', admin.RelatedOnlyFieldListFilter), 'status',)
     ordering = ('catalog_coin__theme',)
+    dynamic_raw_id_fields = ('catalog_coin',)
 
     def get_serie(self, instance):
         return instance.catalog_coin.serie.name
@@ -30,8 +37,3 @@ class CoinAdmin(admin.ModelAdmin):
     def get_year(self, instance):
         return instance.catalog_coin.year
 
-
-# Register your models here.
-admin.site.register(Serie, SerieAdmin)
-admin.site.register(CatalogCoin, CatalogCoinAdmin)
-admin.site.register(Coin, CoinAdmin)

f54a2148ca88c31ff40fc643fe428a7da0b36f9a
Nerevarsoul <keshaeverywhere@gmail.com>
2018-03-21 22:02:46 +0300

    fix errors in auth urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from rest_framework.authtoken import views
 
 
 urlpatterns = [
-    path('', include('social_django.urls', namespace='social')),
-    path(r'^api-token-auth/', views.obtain_auth_token)
+    path('', include('social_django.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]

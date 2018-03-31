f1a085880ba6bd3f9aca309d001daf8a1898e8db
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-03-31 15:48:56 +0300

    edit call api from new method

--- static/assets/js/services/api.js
+++ static/assets/js/services/api.js
@@ -10,13 +10,11 @@ export const fetchCatalogCoins = (serie) => (
 );
 
 export const getCoins = (serie, stat, owner='d3698ffc-abc3-49b2-ac91-77f5273ccc2a') => (
-  fetch(getUrl(serie, stat, owner))
+  callApi('coins/', params={'serie__name': serie, 'owner': owner})
     .then(res => res.json())
 );
 
 export const authPost = (body) => (
-  fetch(`${apiUrl}auth/api-token-auth/`, {
-    method: 'POST',
-    body: data
-  }).then(res => res.json())
+  callApi('auth/api-token-auth/', method='POST', payload=data
+  ).then(res => res.json())
 )

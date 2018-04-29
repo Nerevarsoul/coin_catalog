942c3120041ead165b9d109e5675cd930bc80a66
Nerevarsoul <keshaeverywhere@gmail.com>
2018-04-29 23:54:56 +0300

    fix error with params for call api

--- static/assets/js/services/api.js
+++ static/assets/js/services/api.js
@@ -6,15 +6,15 @@ export const fetchSeries = () => (
 );
 
 export const fetchCatalogCoins = (serie) => (
-  callApi('catalogue/', {params: {'serie__name': serie}})
+  callApi('catalogue/', {'serie__name': serie})
 );
 
 export const getCoins = (serie, stat, owner='d3698ffc-abc3-49b2-ac91-77f5273ccc2a') => (
-  callApi('coins/', {params: {'serie__name': serie, 'owner': owner}})
+  callApi('coins/', {'serie__name': serie, 'owner': owner})
     .then(res => res.json())
 );
 
 export const authPost = (body) => (
-  callApi('auth/api-token-auth/', {params: null, method: 'POST', payload: data}
+  callApi('auth/api-token-auth/', null, 'POST', data}
   ).then(res => res.json())
 )

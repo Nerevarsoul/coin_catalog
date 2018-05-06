8bf67f3aaf0f86a44e382412c665e12755954397
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-05-06 20:39:34 +0300

    add function for render header and body for catalogue component

--- static/assets/js/components/CoinsList.js
+++ static/assets/js/components/CoinsList.js
@@ -2,40 +2,71 @@ import React from 'react';
 import { Table } from 'semantic-ui-react';
 
 
-function renderUser(coin, user) {
+function renderBody(coins, user) {
   if (user) {
     return (
-      <span>
-        <Table.Cell>{ coin['collection'] }</Table.Cell>
-        <Table.Cell>{ coin['exchange'] }</Table.Cell>
-        <Table.Cell>{ coin['wishlist'] }</Table.Cell>
-      </span>
+      <Table.Body>
+        {coins.map((coin, index) =>
+          <Table.Row key={index}>
+            <Table.Cell>{ `${coin['face_value']} ${coin['currency']}` }</Table.Cell>
+            <Table.Cell>{ coin['year'] }</Table.Cell>
+            <Table.Cell>{ coin['theme'] }</Table.Cell>
+            <Table.Cell>{ coin['mint'] }</Table.Cell>
+            <Table.Cell>{ coin['collection'] }</Table.Cell>
+            <Table.Cell>{ coin['exchange'] }</Table.Cell>
+            <Table.Cell>{ coin['wishlist'] }</Table.Cell>
+          </Table.Row>
+        )}
+      </Table.Body>
     )
-  }  
+  } else {
+    return (
+      <Table.Body>
+        {coins.map((coin, index) =>
+          <Table.Row key={index}>
+            <Table.Cell>{ `${coin['face_value']} ${coin['currency']}` }</Table.Cell>
+            <Table.Cell>{ coin['year'] }</Table.Cell>
+            <Table.Cell>{ coin['theme'] }</Table.Cell>
+            <Table.Cell>{ coin['mint'] }</Table.Cell>
+          </Table.Row>
+        )}
+      </Table.Body>
+    )
+  }
 }
 
-
-const CoinsList = ({ coins, user }) => (
-  <Table celled>
-    <Table.Header>
+function renderHeader(user) {
+  if (user) {
+    return (
+      <Table.Row>
+        <Table.HeaderCell>Номинал</Table.HeaderCell>
+        <Table.HeaderCell>Год</Table.HeaderCell>
+        <Table.HeaderCell>Тема</Table.HeaderCell>
+        <Table.HeaderCell>Монетный двор</Table.HeaderCell>
+        <Table.HeaderCell>В коллекции</Table.HeaderCell>
+        <Table.HeaderCell>На обмен</Table.HeaderCell>
+        <Table.HeaderCell>Ищу</Table.HeaderCell>
+      </Table.Row>
+    )
+  } else {
+    return (
       <Table.Row>
         <Table.HeaderCell>Номинал</Table.HeaderCell>
         <Table.HeaderCell>Год</Table.HeaderCell>
         <Table.HeaderCell>Тема</Table.HeaderCell>
         <Table.HeaderCell>Монетный двор</Table.HeaderCell>
       </Table.Row>
+    )
+  }
+}
+
+
+const CoinsList = ({ coins, user }) => (
+  <Table celled>
+    <Table.Header>
+      { renderHeader(user) }
     </Table.Header>
-    <Table.Body>
-      {coins.map((coin, index) =>
-        <Table.Row key={index}>
-          <Table.Cell>{ `${coin['face_value']} ${coin['currency']}` }</Table.Cell>
-          <Table.Cell>{ coin['year'] }</Table.Cell>
-          <Table.Cell>{ coin['theme'] }</Table.Cell>
-          <Table.Cell>{ coin['mint'] }</Table.Cell>
-          {renderUser(coin, user)}
-        </Table.Row>
-      )}
-    </Table.Body>
+    { renderBody(coins, user) }
   </Table>
 )
 

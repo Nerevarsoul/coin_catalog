48d45102d6568314688840362fc4b0223fb58918
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-05-10 22:15:23 +0300

    add president dollars spider

--- spiders/mybot/spiders/usa_coins.py
+++ spiders/mybot/spiders/usa_coins.py
@@ -4,8 +4,8 @@ from ..items import CoinItem, SerieItem
 
 
 class StateQuaterCoins(scrapy.Spider):
-    name = "state_quater"
-    allowed_domains = ["http://collection-coin.ru"]
+    name = 'state_quater'
+    allowed_domains = ['http://collection-coin.ru']
     start_urls = [
         'http://collection-coin.ru/monety-ssha/25-czentov-shtaty-i-territorii-ssha.html',
     ]
@@ -55,3 +55,32 @@ class ParkQuaterCoins(scrapy.Spider):
                 is_issued=year < 2018
             )
             coin.save()
+
+
+class PresidentDollars(scrapy.Spider):
+    name = 'president_dollar'
+    allowed_domains = ['http://collection-coin.ru']
+    start_urls = [
+        'http://collection-coin.ru/monety-ssha/spisok-1-dollar-prezidenty-ssha.html',
+    ]
+
+    def parse(self, response):
+        series = SerieItem(name='1 доллар президенты США', is_active=False, country='США')
+        series = series.save()
+        table = response.xpath("//table[@class='st']")[0]
+        rows = table.xpath(".//tr")
+        for row in rows:
+            items = row.xpath(".//td")
+            year = items[0].xpath(".//text()").extract()[0].strip()
+            if year:
+                theme = items[2].xpath(".//a//text()").extract()[0]
+                coin_p = CoinItem(
+                    year=int(year), country='США', face_value=1, mint='P',
+                    currency='доллар', theme=theme, serie=series
+                )
+                coin_p.save()
+                coin_d = CoinItem(
+                    year=int(year), country='США', face_value=1, mint='D',
+                    currency='доллар', theme=theme, serie=series
+                )
+                coin_d.save()

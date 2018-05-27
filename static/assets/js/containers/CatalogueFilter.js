effd3b6a0306448f6b7cafa336c5fad205df7087
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-05-27 21:06:03 +0300

    add select import

--- static/assets/js/containers/CatalogueFilter.js
+++ static/assets/js/containers/CatalogueFilter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid } from 'semantic-ui-react';
+import { Grid, Select } from 'semantic-ui-react';
 
 import SerieList from '../components/SerieList';
 import CountriesList from '../components/CountriesList';

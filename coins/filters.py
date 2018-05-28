9fcece60cddc99ef64ad31fb21f3e386f05b7969
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-05-28 08:37:05 +0300

    add filter with parent for country

--- coins/filters.py
+++ coins/filters.py
@@ -1,6 +1,7 @@
 import rest_framework_filters as filters
+from django.db.models import Q
 
-from .models import Coin, Serie
+from .models import Coin, Serie, Country
  
 __ALL__ = ('ListingSeriesFilter',)
 
@@ -8,10 +9,11 @@ __ALL__ = ('ListingSeriesFilter',)
 class ListingSeriesFilter(filters.FilterSet):
 
     owner = filters.CharFilter(method='filter_by_owner')
+    country = filters.CharFilter(method='filter_by_country')
 
     class Meta:
         model = Serie
-        fields = ['name', 'country']
+        fields = ('name',)
 
     def filter_by_owner(self, qs, name, value):
         if not value:
@@ -20,5 +22,15 @@ class ListingSeriesFilter(filters.FilterSet):
         qs = qs.filter(
             id__in=Coin.objects.filter(owner=value).select_related('catalog_coin__serie').
                 values_list('catalog_coin__serie__id', flat=True).distinct()
+        )
+        return qs
+
+    def filter_by_country(self, qs, name, value):
+        if not value:
+            return qs
+
+        qs = qs.filter(
+            Q(name=value) |
+            Q(id__in=Country.objects.filter(parent=value).values_list('id', flat=True).distinct())
         )
         return qs
--- coins/models.py
+++ coins/models.py
@@ -33,8 +33,9 @@ class Serie(TimeStampedModel, MPTTModel):
         blank=True, null=True
     )
     parent = TreeForeignKey(
-        'self', null=True, blank=True, 
-        related_name='children', db_index=True, on_delete=models.DO_NOTHING)
+        'self', null=True, blank=True, db_index=True,
+        related_name='children', on_delete=models.DO_NOTHING
+    )
     is_active = models.BooleanField(
         default=True
     )
@@ -241,10 +242,13 @@ class Coin(TimeStampedModel):
         return f'Coin({self.id})'
 
 
-class Country(models.Model):
+class Country(MPTTModel):
     name = models.CharField(primary_key=True, max_length=50)
     flag = models.CharField(max_length=2, blank=True, null=True)
-    union = models.CharField(max_length=20, blank=True, null=True)
+    parent = TreeForeignKey(
+        'self', null=True, blank=True, db_index=True,
+        related_name='children', on_delete=models.DO_NOTHING
+    )
 
     def __str__(self):
         return f'{self.name}'

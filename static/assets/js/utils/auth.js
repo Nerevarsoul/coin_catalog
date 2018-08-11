70fdae25d6d8488d693f501a12193404075afab6
Nerevarsoul <keshaeverywhere@gmail.com>
2018-08-11 12:19:17 +0300

    fix get username method

--- static/assets/js/utils/auth.js
+++ static/assets/js/utils/auth.js
@@ -2,7 +2,11 @@ export const getToken = () => localStorage.getItem('Token');
 export const setToken = (value) => localStorage.setItem('Token', value);
 export const removeToken = () => localStorage.removeItem('Token');
 
-export const getUsername = () => localStorage.getItem('Username');
+export const getUsername = () => {
+  if (getToken) {
+    localStorage.getItem('Username');
+  }
+}
 export const setUsername = (value) => localStorage.setItem('Username', value);
 export const removeUsename = () => localStorage.removeItem('Username');
 

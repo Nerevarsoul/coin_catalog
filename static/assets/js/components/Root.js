d9888f875042473391cc5360f8e6197267917796
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-08-11 12:31:09 +0300

    Moved menu into separate component

--- static/assets/js/components/Root.js
+++ static/assets/js/components/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { Container, Menu, Grid } from 'semantic-ui-react';
 
+import { CoinMenu } from './Menu';
 import { routes, RouteWithSubRoutes } from '../routes';
 
 
@@ -11,26 +12,7 @@ const Root = () => (
         <Grid>
           <Grid.Row>
             <Grid.Column width={3}>
-              <Menu vertical>
-                <Menu.Item link>
-                  <Menu.Menu>
-                    <Menu.Item link><Link to='/registration'>Регистрация</Link></Menu.Item>
-                    <Menu.Item link><Link to='/auth'>Войти</Link></Menu.Item>
-                  </Menu.Menu>
-                </Menu.Item>
-                <Menu.Item link><Link to='/test'>Мой кабинет</Link></Menu.Item>
-                <Menu.Item link>
-                    Монеты
-                    <Menu.Menu>
-                      <Menu.Item link><Link to='/catalogue'>Каталог</Link></Menu.Item>
-                      <Menu.Item link><Link to='/series'>Серии</Link></Menu.Item>
-                      <Menu.Item link><Link to='/coins'>Обменник</Link></Menu.Item>
-                      <Menu.Item link><Link to='/coins'>Монеты на обмен</Link></Menu.Item>
-                      <Menu.Item link><Link to='/coins'>Монеты в поиске</Link></Menu.Item>
-                    </Menu.Menu>
-                </Menu.Item>
-                <Menu.Item link><Link to='/users'>Пользователи</Link></Menu.Item>
-              </Menu>
+              <CoinMenu>
             </Grid.Column>
             <Grid.Column width={13}>
               {routes.map((route, index) => (

ecc917caa08e70e80aac1d5e57d3f721c86098f0
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-09-08 11:37:13 +0300

    add auth validators settings

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -107,6 +107,24 @@ STATICFILES_DIRS = (
 
 AUTH_USER_MODEL = 'accounts.User'
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.vk.VKAppOAuth2',
     'django.contrib.auth.backends.ModelBackend',

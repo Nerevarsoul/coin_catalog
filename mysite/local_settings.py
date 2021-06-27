5eabbee265a94d3dd0df04decc873e641b554613
innokentiyzabirov <keshaeverywhere@gmail.com>
2021-06-27 12:16:01 +0300

    update projects for run

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -1,7 +1,6 @@
 from .settings import *
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -11,7 +10,6 @@ INSTALLED_APPS += (
 )
 
 # Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
